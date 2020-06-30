e05a7ee196f5c94866bf652d6d723824e3ff20fc
v1k1nghawk <thin.king@yandex.ru>
2020-06-30 14:48:22 +0300

    convert leading spaces to tabs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # looper
-* _Purpose:_ A switching (bridge) loop detection by a broadcast storm
+* _Purpose:_ A switching (bridge) loop detection by a broadcast storm.
 * _Usage:_ **looper** [[**-s**|**--silent** [deadline]] | [**-v**|**--verbose**] | [**-h**|**--help**]]
-  
-  **deadline** is a timeout, in seconds, before looper exits (in silent mode)
+
+    **deadline** is a timeout, in seconds, before looper exits (in silent mode).
--- looper.cpp
+++ looper.cpp
@@ -2,7 +2,7 @@
 * File name:            looper.cpp                                                 *
 * Purpose:              A switching (bridge) loop detection by a broadcast storm   *
 * Programmer:           Andrew Artemev                                             *
-* Last modified date:   26.06.2020                                                 *
+* Last modified date:   30.06.2020                                                 *
 \**********************************************************************************/
 
 #include <stdio.h>
@@ -54,31 +54,31 @@ std::unordered_map<std::string, unsigned int> ip_catches;
  * Concurrency control printing with a program name as a prefix
  *
  * @param[in] str_to_cout Printable data
- * @param[in] mode Type of printing (0 - always, 1 - print if it's not silent, 2 - print if it's verbose)
+ * @param[in] mode Type of printing (0 - always, 1 - print if it's not silent mode, 2 - print if it's verbose mode)
  */
 void sync_print(const std::string & str_to_cout, short mode=0)
 {
-   if ( (mode == 0) || ((mode == 1) && (!bsilent)) || ((mode == 2) && (bverbose)) )
-   {
-	  print_mutex.lock();
-      std::cout << PROGNAME << ": " << str_to_cout << std::endl;
-      print_mutex.unlock();
-   }
+    if ( (mode == 0) || ((mode == 1) && (!bsilent)) || ((mode == 2) && (bverbose)) )
+    {
+	print_mutex.lock();
+	std::cout << PROGNAME << ": " << str_to_cout << std::endl;
+	print_mutex.unlock();
+    }
 }
 
 void wait(unsigned int secs)
 {
-   try {
-      boost::this_thread::sleep_for(boost::chrono::seconds(secs));
-   } catch(const boost::thread_interrupted & interrupt) {
-      sync_print("interrupted", 1);
-      terminate_io = 1;
-   }
+    try {
+	boost::this_thread::sleep_for(boost::chrono::seconds(secs));
+    } catch (const boost::thread_interrupted & interrupt) {
+	sync_print("interrupted", 1);
+	terminate_io = 1;
+    }
 }
 
 enum args_code {
     esilent,
-	everbose,
+    everbose,
     ehelp
 };
 
@@ -89,8 +89,8 @@ enum args_code {
  * @return an appropriate code of that argument
  */
 args_code hashargs(const std::string & argtext) {
-	if ( (argtext == "-s") || (argtext == "--silent") ) return esilent;
-	if ( (argtext == "-v") || (argtext == "--verbose") ) return everbose;
+    if ( (argtext == "-s") || (argtext == "--silent") ) return esilent;
+    if ( (argtext == "-v") || (argtext == "--verbose") ) return everbose;
     if ( (argtext == "-h") || (argtext == "--help") ) return ehelp;
     // default
     return ehelp;
@@ -102,146 +102,146 @@ args_code hashargs(const std::string & argtext) {
  */
 void finalizer()
 {
-	// print finds
+    // print finds
 
-	if(terminate_io)
-		sync_print("ERRORS ENCOUNTERED. EXIT.", 1);
+    if (terminate_io)
+	sync_print("ERRORS ENCOUNTERED. EXIT.", 1);
 
-	if(!bsilent)
+    if (!bsilent)
+    {
+	if (ip_catches.size() > 0)
 	{
-	   if(ip_catches.size() > 0)
-	   {
-		   sync_print("Results:", 1);
-		   for(auto & elem : ip_catches)
-		   {
-			  sync_print(boost::lexical_cast<std::string>(elem.second) + " message(s) from IP " + elem.first, 1);
-		   }
-	   }
+	    sync_print("Results:", 1);
+	    for (auto & elem : ip_catches)
+	    {
+		sync_print(boost::lexical_cast<std::string>(elem.second) + " message(s) from IP " + elem.first, 1);
+	    }
 	}
+    }
 
-	if(bloop_found)
-	{
-       sync_print("LOOP FOUND", 1);
-       std::_Exit(EXIT_FAILURE);
-	}
+    if (bloop_found)
+    {
+	sync_print("LOOP FOUND", 1);
+	std::_Exit(EXIT_FAILURE);
+    }
     else
     {
-       sync_print("LOOP NOT FOUND", 1);
-       std::_Exit(EXIT_SUCCESS);
+	sync_print("LOOP NOT FOUND", 1);
+	std::_Exit(EXIT_SUCCESS);
     }
 }
 
 void help(const std::string & binpath)
 {
-	sync_print("version: " + boost::lexical_cast<std::string>(VERSION_MAJOR) + "." + boost::lexical_cast<std::string>(VERSION_MINOR), 0);
-	sync_print("purpose: a switching loop detection by a broadcast storm", 0);
-	sync_print("usage: " + binpath + " [[-s|--silent [deadline]] | [-v|--verbose] | [-h|--help]]", 0);
+    sync_print("version: " + boost::lexical_cast<std::string>(VERSION_MAJOR) + "." + boost::lexical_cast<std::string>(VERSION_MINOR), 0);
+    sync_print("purpose: a switching loop detection by a broadcast storm", 0);
+    sync_print("usage: " + binpath + " [[-s|--silent [deadline]] | [-v|--verbose] | [-h|--help]]", 0);
 
-	std::_Exit(EXIT_SUCCESS);
+    std::_Exit(EXIT_SUCCESS);
 }
 
 
 void tlistener_udp()
 {
-	// wait for broadcast manager
-	while(!bmanager_ready)
-       wait(1);
+    // wait for broadcast manager
+    while (!bmanager_ready)
+	wait(1);
 
-	sync_print("listener: started", 2);
+    sync_print("listener: started", 2);
 
-	bool first_time = 1;
+    bool first_time = 1;
 
-	try {
-	   boost::asio::ip::udp::socket rcv_sock(ios);
+    try {
+	boost::asio::ip::udp::socket rcv_sock(ios);
 
-	   rcv_sock.open(boost::asio::ip::udp::v4());
+	rcv_sock.open(boost::asio::ip::udp::v4());
 
-	   // 0.0.0.0
-	   boost::asio::ip::udp::endpoint rcv_ep = boost::asio::ip::udp::endpoint(boost::asio::ip::address_v4::any(), UDPDEST_PORT);
+	// 0.0.0.0
+	boost::asio::ip::udp::endpoint rcv_ep = boost::asio::ip::udp::endpoint(boost::asio::ip::address_v4::any(), UDPDEST_PORT);
 
-	   rcv_sock.set_option(boost::asio::ip::udp::socket::reuse_address(true));
+	rcv_sock.set_option(boost::asio::ip::udp::socket::reuse_address(true));
 
-	   rcv_sock.bind(rcv_ep);
+	rcv_sock.bind(rcv_ep);
 
-	   // receive data
-       for(;;)
-       {
-    	  if(terminate_io)
-    	     return;
+	// receive data
+	for (;;)
+	{
+	    if(terminate_io)
+		return;
 
-    	  // reaction on catch
-    	  if(!bloop_found)
-    	  {
-    	     // evidence of a detected switching loop
-    	     if(std::accumulate(ip_catches.begin(), ip_catches.end(), 0,
-                [](const unsigned int prev, const std::pair<std::string, unsigned int>& elem)
-                {return prev + elem.second;}) > ip_catches.size())
-    	     {
-    	    	bloop_found = 1;
-    	    	if(bsilent)
-        	       break;
-    	     }
-    	  }
+	    // reaction on catch
+	    if(!bloop_found)
+	    {
+		// evidence of a detected switching loop
+		if(std::accumulate(ip_catches.begin(), ip_catches.end(), 0,
+		    [](const unsigned int prev, const std::pair<std::string, unsigned int>& elem)
+		    {return prev + elem.second;}) > ip_catches.size())
+		{
+		    bloop_found = 1;
+		    if(bsilent)
+			break;
+		}
+	    }
 
-    	  boost::asio::ip::udp::endpoint sender_ep;
-    	  boost::system::error_code err;
+	    boost::asio::ip::udp::endpoint sender_ep;
+	    boost::system::error_code err;
 
-    	  std::vector<char> rcv_payload(snd_payload.size(), '\0');
+	    std::vector<char> rcv_payload(snd_payload.size(), '\0');
 
-	      std::size_t rcv_bytes = 0;
+	    std::size_t rcv_bytes = 0;
 
-	      if (first_time == 1)
-	      {
-			 first_time = 0;
+	    if (first_time == 1)
+	    {
+		first_time = 0;
 
-	    	 sync_print("listener: ready", 2);
+		sync_print("listener: ready", 2);
 
-	         // activate broadcasters
-	    	 listener_ready = 1;
-	      }
+		// activate broadcasters
+		listener_ready = 1;
+	    }
 
-          rcv_bytes = rcv_sock.receive_from(boost::asio::buffer(rcv_payload), sender_ep, 0, err);
+	    rcv_bytes = rcv_sock.receive_from(boost::asio::buffer(rcv_payload), sender_ep, 0, err);
 
-	      if (err && err != boost::asio::error::message_size)
-	         throw boost::system::system_error(err);
+	    if (err && err != boost::asio::error::message_size)
+		throw boost::system::system_error(err);
 
-	      // counting app's broadcast messages
-	      if(std::string(rcv_payload.begin(), rcv_payload.end()) == snd_payload)
-	      {
-	         std::unordered_map<std::string, unsigned int>::iterator ip_cat_iter = ip_catches.find(sender_ep.address().to_string());
+	    // counting app's broadcast messages
+	    if(std::string(rcv_payload.begin(), rcv_payload.end()) == snd_payload)
+	    {
+		std::unordered_map<std::string, unsigned int>::iterator ip_cat_iter = ip_catches.find(sender_ep.address().to_string());
 
-	         if(ip_cat_iter != ip_catches.end())
-	            ip_cat_iter->second++;
-	         else
-	        	ip_catches.insert(std::make_pair(sender_ep.address().to_string(), 1));
+		if(ip_cat_iter != ip_catches.end())
+		    ip_cat_iter->second++;
+		else
+		    ip_catches.insert(std::make_pair(sender_ep.address().to_string(), 1));
 
-		     sync_print("listener: got message from " + boost::lexical_cast<std::string>(sender_ep) +
-		    		 " count " + boost::lexical_cast<std::string>(ip_catches.at(sender_ep.address().to_string())), 1);
-	      }
+		sync_print("listener: got message from " + boost::lexical_cast<std::string>(sender_ep) +
+		    " count " + boost::lexical_cast<std::string>(ip_catches.at(sender_ep.address().to_string())), 1);
+	    }
 
-       }
+	}
 
-	} catch (const std::exception& ex) {
-       sync_print("listener: " + boost::lexical_cast<std::string>(ex.what()), 1);
-       terminate_io = 1;
+    } catch (const std::exception& ex) {
+	sync_print("listener: " + boost::lexical_cast<std::string>(ex.what()), 1);
+	terminate_io = 1;
     }
 
 }
 
 void tbroadcast_udp(boost::asio::ip::udp::endpoint source_ep)
 {
-	// wait for listener
-	for(;;)
-	{
-	   if(terminate_io)
-		  return;
-	   else	if(listener_ready)
-	      break;
-	   else
-	      wait(1);
-	}
+    // wait for listener
+    for (;;)
+    {
+	if(terminate_io)
+	    return;
+	else if(listener_ready)
+	    break;
+	else
+	    wait(1);
+    }
 
-	sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) + ": started", 2);
+    sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) + ": started", 2);
 
     boost::system::error_code snd_err;
 
@@ -254,11 +254,11 @@ void tbroadcast_udp(boost::asio::ip::udp::endpoint source_ep)
     snd_sock.open(protocol, snd_err);
     if (snd_err.value())
     {
-       sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) +
-    		   ": failed to open a sender socket. Error code " +
-			   boost::lexical_cast<std::string>(snd_err.value()) +
-			   ". " + snd_err.message(), 1);
-       terminate_io = 1;
+	sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) +
+	    ": failed to open a sender socket. Error code " +
+	    boost::lexical_cast<std::string>(snd_err.value()) +
+	    ". " + snd_err.message(), 1);
+	terminate_io = 1;
     }
 
     snd_sock.set_option(boost::asio::ip::udp::socket::reuse_address(true));
@@ -267,119 +267,119 @@ void tbroadcast_udp(boost::asio::ip::udp::endpoint source_ep)
     snd_sock.bind(source_ep, snd_err);
     if (snd_err.value())
     {
-       sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) +
-    		   ": failed to bind a source endpoint to a sender socket. Error code " +
-			   boost::lexical_cast<std::string>(snd_err.value()) + ". " + snd_err.message(), 1);
-       terminate_io = 1;
+	sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) +
+	    ": failed to bind a source endpoint to a sender socket. Error code " +
+	    boost::lexical_cast<std::string>(snd_err.value()) + ". " + snd_err.message(), 1);
+	terminate_io = 1;
     }
 
     boost::asio::ip::udp::endpoint dest_ep(boost::asio::ip::address_v4::broadcast(), UDPDEST_PORT);
 
     // send data
     try {
-       sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) + ": sending message to " + boost::lexical_cast<std::string>(dest_ep), 1);
-       snd_sock.send_to(boost::asio::buffer(snd_payload, snd_payload.size()), dest_ep);
-       sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) + ": message has been sent successfully", 2);
+	sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) + ": sending message to " + boost::lexical_cast<std::string>(dest_ep), 1);
+	snd_sock.send_to(boost::asio::buffer(snd_payload, snd_payload.size()), dest_ep);
+	sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) + ": message has been sent successfully", 2);
     } catch (const std::exception& ex) {
-       sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) + ": " + boost::lexical_cast<std::string>(ex.what()), 1);
+	sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) + ": " + boost::lexical_cast<std::string>(ex.what()), 1);
     }
 
     snd_sock.close(snd_err);
     if (snd_err.value())
     {
-       sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) +
-    		   ": failed to close the sender socket. Error code " +
-			   boost::lexical_cast<std::string>(snd_err.value()) +
-			   ". " + snd_err.message(), 1);
-       terminate_io = 1;
+	sync_print("broadcaster on " + boost::lexical_cast<std::string>(source_ep) +
+	    ": failed to close the sender socket. Error code " +
+	    boost::lexical_cast<std::string>(snd_err.value()) +
+	    ". " + snd_err.message(), 1);
+	terminate_io = 1;
     }
 }
 
 void tbroadcast_udp_mng()
 {
-	sync_print("broadcast manager: started", 2);
+    sync_print("broadcast manager: started", 2);
 
-	boost::thread_group tgbroadcastersudp;
+    boost::thread_group tgbroadcastersudp;
 
-	using boost::asio::ip::udp;
+    using boost::asio::ip::udp;
 
-	udp::resolver resolver(ios);
+    udp::resolver resolver(ios);
 
-	sync_print("hostname: " + boost::asio::ip::host_name(), 1);
-	udp::resolver::query query(boost::asio::ip::host_name(), boost::lexical_cast<std::string>(UDPDEST_PORT));
-	udp::resolver::iterator iterator = resolver.resolve(query);
-	udp::resolver::iterator end_iterator;
+    sync_print("hostname: " + boost::asio::ip::host_name(), 1);
+    udp::resolver::query query(boost::asio::ip::host_name(), boost::lexical_cast<std::string>(UDPDEST_PORT));
+    udp::resolver::iterator iterator = resolver.resolve(query);
+    udp::resolver::iterator end_iterator;
 
-	// spawn a dedicated broadcaster on every interface assigned to host name
-	while(iterator != end_iterator)
-	{
-	   udp::endpoint current_iface_ep = *iterator++;
+    // spawn a dedicated broadcaster on every interface assigned to host name
+    while (iterator != end_iterator)
+    {
+	udp::endpoint current_iface_ep = *iterator++;
 
-	   sync_print("IP by hostname: " + current_iface_ep.address().to_string(), 1);
+	sync_print("IP by hostname: " + current_iface_ep.address().to_string(), 1);
 
-	   // on the heap
-	   tgbroadcastersudp.create_thread(boost::bind(tbroadcast_udp, current_iface_ep));
-	}
+	// on the heap
+	tgbroadcastersudp.create_thread(boost::bind(tbroadcast_udp, current_iface_ep));
+    }
 
-	// activate listener
-	bmanager_ready = 1;
+    // activate listener
+    bmanager_ready = 1;
 
-	// wait for derived threads
-	tgbroadcastersudp.join_all();
+    // wait for derived threads
+    tgbroadcastersudp.join_all();
 }
 
 
 int main(int argc, char* argv[])
 {
-	// handler
-	std::set_terminate(finalizer);
+    // handler
+    std::set_terminate(finalizer);
 
-	// analyze app's arguments
-	if ( (argc == 2) || (argc == 3) )
+    // analyze app's arguments
+    if ( (argc == 2) || (argc == 3) )
+    {
+	switch (hashargs(std::string(argv[1])))
 	{
-	   switch (hashargs(std::string(argv[1])))
-	   {
-	      case esilent:
-		      bsilent = 1;
+	    case esilent:
+		bsilent = 1;
 
-		      if(argc == 3)
-		      {
-		         try
-		         {
-		        	deadline = boost::lexical_cast<unsigned int>(argv[2]) * 1000;
-		         }
-		         catch (const boost::bad_lexical_cast & bad_cast)
-		         {
-		        	help(argv[0]);
-		         }
-		      }
-		      break;
-	      case everbose:
-	    	  sync_print("Verbose mode is on");
-		      bverbose = 1;
-		      break;
-	      case ehelp:
-		      help(argv[0]);
-		      break;
-
-	      default:
-		      break;
-	   }
-	}
-	else if (argc > 3)
-	{
+		if(argc == 3)
+		{
+		    try
+		    {
+			deadline = boost::lexical_cast<unsigned int>(argv[2]) * 1000;
+		    }
+		    catch (const boost::bad_lexical_cast & bad_cast)
+		    {
+			help(argv[0]);
+		    }
+		}
+		break;
+	    case everbose:
+		sync_print("Verbose mode is on");
+		bverbose = 1;
+		break;
+	    case ehelp:
 		help(argv[0]);
+		break;
+
+	    default:
+		break;
 	}
+    }
+    else if (argc > 3)
+    {
+	help(argv[0]);
+    }
 
-	// network interaction
-	boost::thread broadcaster(&tbroadcast_udp_mng);
-	boost::thread listener(&tlistener_udp);
+    // network interaction
+    boost::thread broadcaster(&tbroadcast_udp_mng);
+    boost::thread listener(&tlistener_udp);
 
-	broadcaster.join();
-	if(!deadline)        // infinite
-		listener.join();
-	else                 // timed
-	    listener.timed_join(boost::posix_time::milliseconds(deadline));
+    broadcaster.join();
+    if(!deadline)        // infinite
+	listener.join();
+    else                 // timed
+	listener.timed_join(boost::posix_time::milliseconds(deadline));
 
 	// finalize
 	std::terminate();
